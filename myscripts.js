53f68d900b03c09904387f8dde42acebc2a32af3
Johnnie (JD) Stephenson <stepj917@newschool.edu>
2016-11-23 07:02:38 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+                $( ".thefirstdiv" ).hide();
+                $( ".theseconddiv" ).hide();
+                $( ".thethirddiv" ).hide();
+                $( ".background" ).delay(100).fadeIn(10500);
+                $( ".thefirstdiv" ).delay(3500).fadeIn(1500);
+                $( ".theseconddiv" ).delay(6000).fadeIn(1500);
+                $( ".thethirddiv" ).delay(8500).fadeIn(1500);
+                $( ".titleheader" ).delay(10500).fadeIn(1500);
+          
+                    
+                $( ".container" ).delay(15500).fadeIn(4500);
+                $( ".background" ).delay(10500).fadeIn(2500);
+                $( ".finalstatment" ).delay(20500).fadeIn(1500);
+                
+                });
+      
